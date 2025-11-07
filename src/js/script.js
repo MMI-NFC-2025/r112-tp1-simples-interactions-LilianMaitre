4902e33d455af4302e25a4dc5bdadcc132dd0350
LilianMaitre <lilian.maitre@edu.univ-fcomte.fr>
2025-11-07 11:01:17 +0100

    correction du click

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@
 const aMettreEnRouge = document.querySelector ("#a-mettre-en-rouge");
 aMettreEnRouge.style.color = "red";
 
-const enRougeSuiteAClick = document.addEventListener ("#en-rouge-suite-a-click");
-enRougeSuiteAClick.querySelectorAll("click", (evt) => {
+const enRougeSuiteAClick = document.querySelector ("#en-rouge-suite-a-click");
+enRougeSuiteAClick.addEventListener("click", (evt) => {
     enRougeSuiteAClick.style.color = "red"; 
 });
 
