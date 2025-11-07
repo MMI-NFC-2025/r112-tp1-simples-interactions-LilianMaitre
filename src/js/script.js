d8ea5e1bb0f22bb9f57ecd58fd5634aa585b4270
LilianMaitre <lilian.maitre@edu.univ-fcomte.fr>
2025-11-07 10:59:26 +0100

    ajustements

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,12 @@
 // TODO
-const aMettreEnRouge = document.querySelector ("#a-mettre-en-rouge")
-aMettreEnRouge.style.color = "red"
+const aMettreEnRouge = document.querySelector ("#a-mettre-en-rouge");
+aMettreEnRouge.style.color = "red";
 
-const enRougeSuiteAClick = document.addEventListener ("#en-rouge-suite-a-click")
-enRougeSuiteAClick.querySelectorAll("click", (evt) => {enRougeSuiteAClick.style.color = "red" });
+const enRougeSuiteAClick = document.addEventListener ("#en-rouge-suite-a-click");
+enRougeSuiteAClick.querySelectorAll("click", (evt) => {
+    enRougeSuiteAClick.style.color = "red"; 
+});
 
-const collH2 = document.querySelectorAll("h2")
-collH2.forEach((elm)=>{elm.addEventListener("click",(evt)=>{evt.target.style.color = "red"; }); });
+const collH2 = document.querySelectorAll("h2");
+collH2.forEach((elm)=>{elm.addEventListener("click",(evt)=>{
+    evt.target.style.color = "red"; }); });
