3b70c2eceb7dda3295806ec7e16e6f8835067f9d
LilianMaitre <lilian.maitre@edu.univ-fcomte.fr>
2025-11-07 10:34:56 +0100

    réagir à un click

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
 // TODO
 aMettreEnRouge = document.querySelector ("#a-mettre-en-rouge")
 aMettreEnRouge.style.color = "red"
+
+enRougeSuiteAClick = document.querySelector ("#en-rouge-suite-a-click")
+enRougeSuiteAClick.querySelectorAll("click", (evt) => {enRougeSuiteAClick.style.color = "red" });
