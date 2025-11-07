f8b46df8a5bdac2177963ed9119cfbc9c9a8a4d7
LilianMaitre <lilian.maitre@edu.univ-fcomte.fr>
2025-11-07 10:54:27 +0100

    click sur les h2

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,8 @@
 aMettreEnRouge = document.querySelector ("#a-mettre-en-rouge")
 aMettreEnRouge.style.color = "red"
 
-enRougeSuiteAClick = document.addEventListener ("#en-rouge-suite-a-click")
+const enRougeSuiteAClick = document.addEventListener ("#en-rouge-suite-a-click")
 enRougeSuiteAClick.querySelectorAll("click", (evt) => {enRougeSuiteAClick.style.color = "red" });
 
+collH2 = document.querySelectorAll("h2")
+collH2.forEach((elm)=>{elm.addEventListener("click",(evt)=>{evt.target.style.color = "red"; }); });
