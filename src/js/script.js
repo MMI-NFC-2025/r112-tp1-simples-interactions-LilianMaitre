e646424d3cc1089de3f0a6411be05a9ea690c1b5
LilianMaitre <lilian.maitre@edu.univ-fcomte.fr>
2025-11-07 10:55:09 +0100

    ajout des const

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
 // TODO
-aMettreEnRouge = document.querySelector ("#a-mettre-en-rouge")
+const aMettreEnRouge = document.querySelector ("#a-mettre-en-rouge")
 aMettreEnRouge.style.color = "red"
 
 const enRougeSuiteAClick = document.addEventListener ("#en-rouge-suite-a-click")
 enRougeSuiteAClick.querySelectorAll("click", (evt) => {enRougeSuiteAClick.style.color = "red" });
 
-collH2 = document.querySelectorAll("h2")
+const collH2 = document.querySelectorAll("h2")
 collH2.forEach((elm)=>{elm.addEventListener("click",(evt)=>{evt.target.style.color = "red"; }); });
